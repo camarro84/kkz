1a81208de37ce52cde668834298f9c5121575b01
camarro84 <camarro84@gmail.com>
2025-03-30 19:20:20 +0200

    upload files
    index+src

---
+++ src/js/script.js
@@ -0,0 +1,9 @@
+function toggleCode(id) {
+    var element = document.getElementById(id);
+    if (element.style.display === "none") {
+      element.style.display = "block";
+    } else {
+      element.style.display = "none";
+    }
+  }
+  
